4f52837beead16050a477cb4ac69f52c31404f3d
Jebu Ittiachen <jebui@yahoo.com>
2010-09-01 14:54:28 +0530

    fix erlang interface

--- src/trends_manager/trends.c
+++ src/trends_manager/trends.c
@@ -1,4 +1,6 @@
 #include "erl_nif.h"
+#include "erl_driver.h"
+
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -25,70 +27,61 @@ static int my_enif_get_string(ErlNifEnv *env, ERL_NIF_TERM list, char* buf) {
   return 0;
 }
 
-static ERL_NIF_TERM nif_test(ErlNifEnv *env, ERL_NIF_TERM tweet) {
-  char str[1024];
-  memset(str, 0, 1024);
+ERL_NIF_TERM nif_test(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
+  ErlNifBinary tweet;
+  ErlNifBinary user;
+  char tweet_str[1024];
+  char tweet_user[1024];
+  memset(tweet_str, 0, 1024);
+  memset(tweet_user, 0, 1024);
 
-  ERL_NIF_TERM head, tail;
-  enif_get_list_cell(env, tweet, &head, &tail);
-
-  printf("good");
-  if (enif_is_atom(env, tweet))
-    printf("atom");
-  else if (enif_is_binary(env, tweet))
-    printf("binary");
-  else if (enif_is_ref(env, tweet))
-    printf("ref");
-//  else if (enif_is_tuple(env, tweet))
-//    printf("tuple");
-//  else if (enif_is_list(env, tweet))
-//    printf("list");
-  else
-    printf("unknown");
-
-  //if (enif_get_string(env, tweet, str, 1024, ERL_NIF_UTF8) < 1) {
-  if (my_enif_get_string(env, tweet, str) < 0) {
-    printf("ERROR: could not convert the tweet from erlang term to const char*\n");
+  if (enif_inspect_binary(env, argv[0], &tweet) &&
+      enif_inspect_binary(env, argv[1], &user)) {
+    memcpy(tweet_str, tweet.data, tweet.size);
+    memcpy(tweet_user, user.data, user.size);
+    return enif_make_tuple2(env, enif_make_string(env, tweet_str, ERL_NIF_LATIN1),
+                                 enif_make_string(env, tweet_user, ERL_NIF_LATIN1));
+  } else 
     return enif_make_string(env, "ERROR", ERL_NIF_LATIN1);
-  }
-
-  return enif_make_string(env, str, ERL_NIF_LATIN1);
 }
 
-static int nif_getkeywords(ErlNifEnv *env, ERL_NIF_TERM user_name, ERL_NIF_TERM tweet, ERL_NIF_TERM *keywords_array) {
+ERL_NIF_TERM nif_getkeywords(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
+  ErlNifBinary tweet;
+  ErlNifBinary user;
+  
   char tweet_str[1024];
-  memset(tweet_str, 0, 1024);
-  if (my_enif_get_string(env, tweet, tweet_str) < 0) {
-    printf("ERROR: could not convert the tweet from erlang term to const char*\n");
-    return -1;
-  }
-
   char user_name_str[255];
   memset(user_name_str, 0, 255);
-  if (my_enif_get_string(env, user_name, user_name_str) < 0) {
-    printf("ERROR: could not convert the user_name from erlang term to const char*\n");
-    return -1;
-  }
+  memset(tweet_str, 0, 1024);
+  
+  if (enif_inspect_binary(env, argv[0], &user) &&
+      enif_inspect_binary(env, argv[1], &tweet)) {
+    memcpy(tweet_str, tweet.data, tweet.size);
+    memcpy(user_name_str, user.data, user.size);
+  } else
+    return enif_make_atom(env, "error");
+
 
   char keywords[1024];
   memset(keywords, 0, 1024);
   if (SubmitTweet((const char*) user_name_str, (const char *) tweet_str, (char*) keywords) < 0) {
-    printf("ERROR: could not submit tweet to keyword extracter\n");
-    return -1;
+    return enif_make_atom(env, "submit_error");
   }
 
+  ERL_NIF_TERM return_list = enif_make_list(env, 0);
   char *start = keywords;
-  char *end = NULL;
+  char *end = " ";
   while (start && end && *end != '\0') {
     end = strstr(start, ",");
     if (!end)
       break;
     *end = '\0';
-    enif_make_string(env, (const char*) start, ERL_NIF_LATIN1);
+    return_list = enif_make_list_cell(env, enif_make_string(env, (const char*) start, ERL_NIF_LATIN1), return_list);
     *end = ',';
     start = end + 1;
   }
-  return 0;
+
+  return return_list;
 }
 
 static ERL_NIF_TERM nif_gettrends(ErlNifEnv *env, ERL_NIF_TERM user_name) {
@@ -110,7 +103,8 @@ static ERL_NIF_TERM nif_gettrends(ErlNifEnv *env, ERL_NIF_TERM user_name) {
   return enif_make_string(env, trends_str, ERL_NIF_LATIN1);
 }
 
-static int nif_init_c(ErlNifEnv *env) {
+int nif_init_c(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
+  printf("good2");
   if (Init() < 0)
     return -1;
 
@@ -129,14 +123,6 @@ static int nif_submit(ErlNifEnv *env) {
 
   if (!keywords_array)
     return -1;
-
-  if (nif_getkeywords(env,
-                      enif_make_string(env, "tantricninja", ERL_NIF_LATIN1),
-                      enif_make_string(env, "Contigous Caps will be counted. So will be contiguous nonstopwords", ERL_NIF_LATIN1),
-                      keywords_array) < 0) {
-    printf("Error\n");
-    return -1;
-  }
 
   return 0;
 }
@@ -157,10 +143,10 @@ static ErlNifFunc nif_funcs[] =
   {"init_c", 0, nif_init_c},
   {"submit", 0, nif_submit},
   {"trends", 0, nif_trends},
-  {"deinit_c", 0, nif_deinit_c},
-  {"getkeywords", 3, nif_getkeywords},
+  //{"deinit_c", 0, nif_deinit_c},
+  {"getkeywords", 2, nif_getkeywords},
   {"gettrends", 1, nif_trends},
-  {"test", 1, nif_test},
+  {"test", 2, nif_test},
 };
 ERL_NIF_INIT(trends, nif_funcs, NULL, NULL, NULL, NULL)
 
--- src/trends_manager/trends.erl
+++ src/trends_manager/trends.erl
@@ -1,5 +1,5 @@
 -module(trends).
--export([init/0, init_c/0, submit/0, trends/0, deinit_c/0, getkeywords/3, gettrends/1, test/1]).
+-export([init/0, init_c/0, submit/0, trends/0, deinit_c/0, getkeywords/2, gettrends/1, test/2]).
 init() ->
   erlang:load_nif("./trends", 0).
 init_c() ->
@@ -10,9 +10,9 @@ trends() ->
   "NIF library not loaded".
 deinit_c() ->
   "NIF library not loaded".
-getkeywords(_user, _tweet, _keywords) ->
+getkeywords(_user, _tweet) ->
   "NIF library not loaded".
 gettrends(_user) ->
   "NIF library not loaded".
-test(_tweet) ->
+test(_tweet, _user) ->
   "NIF library not loaded".
