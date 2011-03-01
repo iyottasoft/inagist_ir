4d4a4d24ec020acadd10167364f37f974527f9c9
balaji <balaji@valainool.(none)>
2011-03-01 16:51:05 +0530

    writing output to an html file

--- src/trends_manager/t_lang_detect.cc
+++ src/trends_manager/t_lang_detect.cc
@@ -31,9 +31,14 @@ int Init(std::string& root_dir) {
 }
 
 int TestLangForHandle(std::string& handle, const char* expected_lang,
-                      unsigned int& tweets_num, unsigned int& detected_num, unsigned int& undefined_num) {
+                      unsigned int& tweets_num, unsigned int& detected_num,
+                      unsigned int& undefined_num,
+                      const unsigned int output_type, std::ostream& ostream_ptr) {
 
-  std::cout << "testing tweets of handle: " << handle << std::endl;
+  if (output_type == 1) {
+    ostream_ptr << "<tr><td><br/>" << std::endl;
+  }
+  ostream_ptr << "handle: " << handle << std::endl;
   char tweets_buffer[MAX_BUFFER_LEN];
   unsigned int tweets_len = 0;
   if (GetTestTweets(handle.c_str(), MAX_BUFFER_LEN, tweets_buffer, &tweets_len) < 0) {
@@ -96,16 +101,18 @@ int TestLangForHandle(std::string& handle, const char* expected_lang,
                     (char *) buffer2, MAX_BUFFER_LEN,
                     (char *) buffer3, MAX_BUFFER_LEN,
                     (char *) buffer4, MAX_BUFFER_LEN)) < 0) {
+      if (output_type == 1)
+        ostream_ptr << "</tr></td><br/>" << std::endl;
       return -1;
     }
-    std::cout << "Tweet: " << tweet_start << std::endl;
-    std::cout << "expected lang: " << expected_lang << std::endl;
-    std::cout << "script: " << script << std::endl;
-    std::cout << "lang guess 1: " << buffer1 << std::endl;
-    std::cout << "lang guess 2: " << buffer2 << std::endl;
+    ostream_ptr << "Tweet: " << tweet_start << std::endl;
+    ostream_ptr << "expected lang: " << expected_lang << std::endl;
+    ostream_ptr << "script: " << script << std::endl;
+    ostream_ptr << "lang guess 1: " << buffer1 << std::endl;
+    ostream_ptr << "lang guess 2: " << buffer2 << std::endl;
     if ((strcmp(expected_lang, buffer1) == 0) ||
-        ((strcmp(script, buffer1) == 0) &&
-         (strcmp("en", buffer1) != 0))) {
+        ((strcmp(expected_lang, script) == 0) &&
+         (strcmp("en", script) != 0))) {
       detected_num++;
     } else if ((strlen(buffer1) < 2) ||
                (strcmp("RR", buffer1) == 0) ||
@@ -119,17 +126,23 @@ int TestLangForHandle(std::string& handle, const char* expected_lang,
   tweet_start = NULL;
   tweet_end = NULL;
 
-  std::cout << std::endl << "tweets: " << tweets_num \
-            << " detected: " << detected_num \
-            << " undefined: " << undefined_num << std::endl;
+  if (output_type == 1)
+    ostream_ptr << "<hr/>" << std::endl;
+
+  ostream_ptr << std::endl << "tweets: " << tweets_num \
+      << " detected: " << detected_num \
+      << " undefined: " << undefined_num << std::endl;
+
+  if (output_type == 1)
+    ostream_ptr << "</tr></td><br/>" << std::endl;
 
   return 0;
 }
 
 int main(int argc, char* argv[]) {
 
-  if (argc != 2) {
-    std::cout << "Usage: " << argv[0] << " <config_file_name>\n";
+  if (argc != 2 && argc != 3) {
+    std::cout << "Usage: " << argv[0] << " <config_file_name> [output_html_file]\n";
     return -1;
   }
 
@@ -139,89 +152,130 @@ int main(int argc, char* argv[]) {
   if (!ifs.is_open()) {
     std::cout << "ERROR: could not open config file " << config_file_name << std::endl;
     return -1;
+  }
+
+  std::ofstream ofs;
+  std::ostream* ostream_ptr;
+  std::string output_html_file_name;
+  unsigned int output_type = 0;
+  if (argc == 3) {
+    output_html_file_name = std::string(argv[2]);
+    ofs.open(output_html_file_name.c_str());
+    output_type = 1;
+    ostream_ptr = &ofs;
+    *ostream_ptr << "<html>\n<head>\n" \
+                 << "<title>Inagist Language Detection</title>\n" \
+                 << "</head>\n<body>\n<table>" << std::endl;
+    ofs.close();
+    ofs.open(output_html_file_name.c_str(), std::ios_base::app);
+    output_type = 1;
+    ostream_ptr = &ofs;
   } else {
+    ostream_ptr = &std::cout;
+  }
 
-    std::string arguments(argv[0]);
-    std::string::size_type loc = arguments.find("bin", 0);
-    std::string root_dir;
-    if (loc != std::string::npos) {
-      loc-=1;
-      root_dir.assign(argv[0], loc);
+  std::string arguments(argv[0]);
+  std::string::size_type loc = arguments.find("bin", 0);
+  std::string root_dir;
+  if (loc != std::string::npos) {
+    loc-=1;
+    root_dir.assign(argv[0], loc);
+  }
+
+  if (Init(root_dir) < 0) {
+    std::cout << "ERROR: could not initialize keywords extract\n";
+    return -1;
+  }
+
+  std::string line;
+  std::string key;
+  std::string value;
+  int line_count = 0;
+  std::string lang;
+  std::string handles_file_name;
+  std::string output_tweets_file_name;
+  std::string output_corpus_file_name;
+  unsigned int tweets_num = 0;
+  unsigned int detected_num = 0;
+  unsigned int undefined_num = 0;
+  unsigned int total_tweets_num = 0;
+  unsigned int total_detected_num = 0;
+  unsigned int total_undefined_num = 0;
+  char debug_str[255];
+  memset(debug_str, '\0', 255);
+  std::set<std::string> debug_str_set;
+  while (getline(ifs, line)) {
+    line_count++;
+    loc = line.find("=", 0);
+    if (loc == std::string::npos) {
+      std::cout << "ERROR: invalid config file entry\n";
+      break;
     }
-
-    if (Init(root_dir) < 0) {
-      std::cout << "ERROR: could not initialize keywords extract\n";
-      return -1;
+    key.assign(line.c_str(), loc);
+    value.assign(line.c_str(), loc+1, (line.length()-loc-1));
+    if (key.compare(0, 4, "lang") == 0) {
+      lang = value;
+    } else if (key.compare(0, 7, "handles") == 0) {
+      handles_file_name = value;
+    } else if (key.compare(0, 6, "corpus") == 0) {
+      output_corpus_file_name = value;
+    } else if (key.compare(0, 6, "tweets") == 0) {
+      output_tweets_file_name = value;
     }
-
-    std::string line;
-    std::string key;
-    std::string value;
-    int line_count = 0;
-    std::string lang;
-    std::string handles_file_name;
-    std::string output_tweets_file_name;
-    std::string output_corpus_file_name;
-    unsigned int tweets_num = 0;
-    unsigned int detected_num = 0;
-    unsigned int undefined_num = 0;
-    unsigned int total_tweets_num = 0;
-    unsigned int total_detected_num = 0;
-    unsigned int total_undefined_num = 0;
-    char debug_str[255];
-    memset(debug_str, '\0', 255);
-    std::set<std::string> debug_str_set;
-    while (getline(ifs, line)) {
-      line_count++;
-      loc = line.find("=", 0);
-      if (loc == std::string::npos) {
-        std::cout << "ERROR: invalid config file entry\n";
-        break;
-      }
-      key.assign(line.c_str(), loc);
-      value.assign(line.c_str(), loc+1, (line.length()-loc-1));
-      if (key.compare(0, 4, "lang") == 0) {
-        lang = value;
-      } else if (key.compare(0, 7, "handles") == 0) {
-        handles_file_name = value;
-      } else if (key.compare(0, 6, "corpus") == 0) {
-        output_corpus_file_name = value;
-      } else if (key.compare(0, 6, "tweets") == 0) {
-        output_tweets_file_name = value;
-      }
-      if (line_count == 4) {
-        std::ifstream hfs(handles_file_name.c_str());
-        if (!hfs.is_open()) {
-          std::cout << "ERROR: could not open handles file: " << handles_file_name \
-                    << " for lang: " << lang << std::endl;
-        } else {
-          std::string handle;
-          getline(hfs, handle);
-          hfs.close();
-          if (TestLangForHandle(handle, lang.c_str(), tweets_num, detected_num, undefined_num) < 0) {
-            std::cout << "ERROR: TestLangForHandle failed for lang: " \
-                      << lang << "on handle: " << handle << std::endl;
-          }
-          total_tweets_num += tweets_num;
-          total_detected_num += detected_num;
-          total_undefined_num += undefined_num;
-          memset(debug_str, '\0', 255);
-          sprintf(debug_str, "%s %u %u %u", lang.c_str(), tweets_num, detected_num, undefined_num);
-          debug_str_set.insert(std::string(debug_str));
+    if (line_count == 4) {
+      std::ifstream hfs(handles_file_name.c_str());
+      if (!hfs.is_open()) {
+        std::cout << "ERROR: could not open handles file: " << handles_file_name \
+                  << " for lang: " << lang << std::endl;
+      } else {
+        std::string handle;
+        getline(hfs, handle);
+        hfs.close();
+        tweets_num = 0;
+        detected_num = 0;
+        undefined_num = 0;
+        if (TestLangForHandle(handle, lang.c_str(),
+                              tweets_num, detected_num, undefined_num,
+                              output_type, *ostream_ptr) < 0) {
+          std::cout << "ERROR: TestLangForHandle failed for lang: " \
+                    << lang << "on handle: " << handle << std::endl;
         }
-        line_count = 0;
+        total_tweets_num += tweets_num;
+        total_detected_num += detected_num;
+        total_undefined_num += undefined_num;
+        memset(debug_str, '\0', 255);
+        sprintf(debug_str, "%s %u %u %u", lang.c_str(), tweets_num, detected_num, undefined_num);
+        debug_str_set.insert(std::string(debug_str));
       }
+      line_count = 0;
     }
-    ifs.close();
+  }
+  ifs.close();
 
-    std::set<std::string>::iterator set_iter;
-    for (set_iter = debug_str_set.begin(); set_iter != debug_str_set.end(); set_iter++)
-      std::cout << *set_iter << std::endl;
+  if (output_type == 1) {
+    *ostream_ptr << "</table>\n<br/>" << std::endl;
+  }
+  *ostream_ptr << "Summary:" << std::endl;
+  std::set<std::string>::iterator set_iter;
+  for (set_iter = debug_str_set.begin(); set_iter != debug_str_set.end(); set_iter++) {
+    *ostream_ptr << *set_iter << std::endl;
+    if (output_type == 1) {
+      *ostream_ptr << "<br/>" << std::endl;
+    }
+  }
+  debug_str_set.clear();
 
-    std::cout << std::endl << "total tweets: " << total_tweets_num \
-              << " total detected: " << total_detected_num \
-              << " total undefined: " << total_undefined_num << std::endl;
+  *ostream_ptr << std::endl << "total tweets: " << total_tweets_num \
+            << " total detected: " << total_detected_num \
+            << " total undefined: " << total_undefined_num << std::endl;
 
+  if (output_type == 1) {
+    *ostream_ptr << "</body>\n</html>" << std::endl;
+  }
+
+  if (argc == 3) {
+    flush(*ostream_ptr);
+    ofs.close();
   }
 
   return 0;
