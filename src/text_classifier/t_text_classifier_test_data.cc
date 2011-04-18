c5fc161d9ab2903a25ec20a092057a6308996019
balaji <balaji@valainool.(none)>
2011-04-18 12:21:33 +0530

    adding keytuples config as an input parameter

--- src/text_classifier/t_text_classifier_test_data.cc
+++ src/text_classifier/t_text_classifier_test_data.cc
@@ -1,23 +1,25 @@
 #include "text_classifier.h"
 #include <iostream>
+#include <cstring>
 #include <cstdlib>
 
 using namespace inagist_classifiers;
 
 int main(int argc, char* argv[]) {
 
-  if (argc != 4 && argc != 5) {
-    std::cout << "Usage: " << argv[0] << " <classifier_config> <input_type> <output_type> [output_file]\n";
+  if (argc != 5 && argc != 6) {
+    std::cout << "Usage: " << argv[0] << " <classifier_config> <keytuples_config> <input_type> <output_type> [output_file]\n";
     std::cout << "input_type:\n\t0 - twitter timeline\n\t1 - random selection of training sources\n\t2 - all training sources (all handles)\noutput_type:\n\t0 - stdout\n\t1 - class frequency file\n\t2 - html version\n";
     return -1;
   }
 
   const char* config_file = argv[1];
-  unsigned int input_type = atoi(argv[2]);
-  unsigned int output_type = atoi(argv[3]);
+  const char* keytuples_config_file = argv[2];
+  unsigned int input_type = atoi(argv[3]);
+  unsigned int output_type = atoi(argv[4]);
   char* output_file = NULL;
-  if (5 == argc) {
-    output_file = argv[4];
+  if (6 == argc) {
+    output_file = argv[5];
   }
 
   TextClassifier *tc = (TextClassifier *) new TextClassifier();
@@ -27,7 +29,10 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
-  if (tc->InitDependencies() < 0) {
+  int my_argc = 1;
+  char my_argv[1][255];
+  strcpy((char *) my_argv[0], keytuples_config_file);
+  if (tc->InitDependencies(my_argc, (char **) my_argv) < 0) {
     std::cerr << "ERROR: could not initialize dependencies for text classifier\n";
     return -1;
   }
